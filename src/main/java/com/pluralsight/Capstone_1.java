442bde9e1491d7f2fb60e72490883cac0b6bffeb
Cesar <Perezce41@gmail.com>
2024-10-15 15:13:46 -0700

    finished up everything in ledger but reports, made sure ledger loops until going home and exiting, refactored code to make it look cleaner

--- src/main/java/com/pluralsight/Capstone_1.java
+++ src/main/java/com/pluralsight/Capstone_1.java
@@ -1,8 +1,6 @@
 package com.pluralsight;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import javax.sound.sampled.Line;
+import java.io.*;
 import java.lang.reflect.Array;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -12,132 +10,151 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Capstone_1 {
+    //psv innit and put all 17-31 in method
 
     static Scanner input = new Scanner(System.in);
+    static FileReader fileReader;
+
+    static {
+        try {
+            fileReader = new FileReader("transactions.csv");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static BufferedReader buffReader = new BufferedReader(fileReader);
+    static FileWriter writer;
+
+    static {
+        try {
+            writer = new FileWriter("transactions.csv", true);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static List<String> depositList = new ArrayList<>();
+    static List<String> paymentList = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         DateTimeFormatter.ofPattern("yyyy/MM/dd");
-        List<String[]> depositList = new ArrayList<>();
-        List<String[]> paymentList = new ArrayList<>();
+
+        //read file to add stuff to list then parse index 4 to >0 to check if payment if not add to deposit
+       //String[] arrayToList = "transactions.csv".split(",");
+        String line;
+
+        while ((line = buffReader.readLine()) != null) {
+            String[] transactionArray = line.split("\\|");
+            if(Double.parseDouble(transactionArray[4]) > 0){
+                depositList.add(line);
+            }else {
+                paymentList.add(line);
+            }
+        }
 
         //First I'm making my home menu and displaying options for the user to select.
         //have to restructure make home menu a static method maybe
         //make sure ledger gets updated as i do payments i can do this by making an arraylist as the transactions get put in.
         //there should be a screen for home ledger then reports.
         //close buffered writer after deposit as it needs to read so it doesnt overwrite the payment.
-            boolean home = true;
-            while(home){
-                System.out.println("Hello! Welcome to the CLI finance App ");
-                System.out.println("-------------------------------------");
-                System.out.println("Please select one our options below!");
-                System.out.println("(D) Add Deposit \n(P) Make Payment (Debit) \n(L) Displays Ledger Menu \n(X) Exit Application");
-                String selection1 = input.nextLine();
-                //make seperate methods for cases body is becoming enlarged
+        boolean home = true;
+        boolean ledger = true;
+
+
+        while (home) {
+            System.out.println("Hello! Welcome to the CLI finance App ");
+            System.out.println("-------------------------------------");
+            System.out.println("Please select one our options below!");
+            System.out.println("(D) Add Deposit \n(P) Make Payment (Debit) \n(L) Displays Ledger Menu \n(X) Exit Application");
+            String selection1 = input.nextLine();
+            //make seperate methods for cases body is becoming enlarged
+
 
             switch (selection1.toUpperCase()) {
                 case "D":
-                    //in this case im writing a edit onto a csv file based on the info they put in, It should all be in one main csv
+                    // in this case im writing a edit onto a csv file based on the info they put in, It should all be in one main csv
                     //Make sure to append or else im rewriting the file every time
-                    try {
-                        System.out.println("Im going to need your deposit information");
-                        System.out.println("Please enter all the info in this exact format (MM/dd/yyyy)|(Military time e.g 17:00)| description | amount");
-                        System.out.println("e.g 2023-04-15|11:15:00|Invoice 1001 paid|Joe|1500.00 ");
-                        String depositInfo = input.nextLine();
-                        //split using |
-                        String[] depositArray = depositInfo.split("\\|");
-
-                        depositList.add(depositArray);
-
-                        FileWriter writeon = new FileWriter( "transactions.csv", true);
-
-                        writeon.write(Arrays.toString(depositArray) + " " + "Added on" + "\n");
-                        writeon.close();
-
-                    } catch (RuntimeException e) {
-                        throw new RuntimeException(e);
-                    }
+                    handleDeposit();
                     break;
                 case "P":
-                    try{
-                    System.out.println("Im going to need your debit information");
-                    System.out.println("Please enter your information in the following format");
-                    System.out.println("2023-04-15|10:13:25|ergonomic keyboard|Amazon|-89.50");
-                    String paymentInfo = input.nextLine();
-
-                    FileWriter writeon = new FileWriter( "transactions.csv",  true);
-                    String[] paymentArray = paymentInfo.split("\\|");
-
-                    paymentList.add(paymentArray);
-
-                    writeon.write(Arrays.toString(paymentArray) + " " + "Added on" + "\n") ;
-                    writeon.close();
-
-                    } catch (RuntimeException e) {
-                        throw new RuntimeException(e);
-                    }
+                    handlePayments();
                     break;
-                case "L": home = false;
+                case "L":
+                    home = false;
                     //should open a ledger screen pressing a should print all transactions etc
                     //there should be a function that sorts all transactions and stuff
                     System.out.println("Moving to Ledger");
                     try {
-                        System.out.println("______________________");
-                        Thread.sleep(1000);
+                        Thread.sleep(500);
                     } catch (InterruptedException e) {
                         throw new RuntimeException(e);
                     }
+                    while(ledger) { //should move to seperate function
+                        System.out.println("______________________");
+                        System.out.println("Welcome to the ledger, Please select an option: ");
+                        System.out.println("(A) Display All reports.\n(D) Display Deposits into the account\n(P) Display all negative entries (payments)\n(R) Display Reports\n(H) Navigate back to home.");
+                        String ledgerSelection = input.nextLine();
 
-                    System.out.println("Welcome to the ledger, Please select an option: ");
-                    System.out.println("(A) Display All reports.\n(D) Display Deposits into the account\n(P) Display all negative entries (payments)\n(R) Display Reports\n(H) Navigate back to home.");
-                    String ledgerSelection = input.nextLine();
 
-                    switch (ledgerSelection.toUpperCase()){
-                        case "A":
-                            try {
+                        switch (ledgerSelection.toUpperCase()) {
+                            case "A":
+                                try {
 
-                                FileReader fileread = new FileReader("transactions.csv");
-                                BufferedReader bufReader1 = new BufferedReader(fileread);
+                                    FileReader fileread = new FileReader("transactions.csv");
+                                    BufferedReader bufReader1 = new BufferedReader(fileread);
 
-                                String readPut;
+                                    String readPut;
 
-                                while ((readPut = bufReader1.readLine()) != null) {
-                                    System.out.println(readPut);
+                                    while ((readPut = bufReader1.readLine()) != null) {
+                                        System.out.println(readPut);
+                                    }
+                                    bufReader1.close();
+                                } catch (IOException e) {
+                                    e.printStackTrace();
                                 }
-                                bufReader1.close();
-                            }catch(IOException e) {
-                                e.printStackTrace();
-                            }break;
-                        case "D":
-                            try {
+                                break;
+                            case "D":
+                                try {
 
-                            FileReader fileread = new FileReader("transactions.csv");
-                            BufferedReader bufferedRead2 = new BufferedReader(fileread);
+                                    FileReader fileread = new FileReader("transactions.csv");
+                                    BufferedReader bufferedRead2 = new BufferedReader(fileread);
+                                    while ((line = bufferedRead2.readLine()) != null) {
+                                        String[] transactionArray = line.split("\\|");
+                                        if (Double.parseDouble(transactionArray[4]) > 0) {
+                                            System.out.println(line);
+                                        }
 
-                            while (bufferedRead2.readLine() != null) {
-                                System.out.println(depositList);
-                            }
-                            } catch(IOException e) {
-                                e.printStackTrace();
+                                    }
+                                } catch (IOException e) {
+                                    e.printStackTrace();
                                 }       //might have to make a method for filereader would make more sense than writing it out everytime
-                            break;
-                        case "P":
+                                break;
+                            case "P":
                                 try {
                                     FileReader fileReadPay = new FileReader("transactions.csv");
                                     BufferedReader buffReaderPay = new BufferedReader(fileReadPay);
 
-                                    while (buffReaderPay.readLine() != null) {
-                                        System.out.println(paymentList);
+                                    while ((line = buffReaderPay.readLine()) != null) {
+                                        String[] transactionArray = line.split("\\|");
+                                        if (Double.parseDouble(transactionArray[4]) < 0) {
+                                            System.out.println(line);
+                                        }
                                     }
-                                }catch (IOException e){
+                                } catch (IOException e) {
                                     e.printStackTrace();
                                 }
                                 break;
-                        case "H":
-                            home = true;
-                    }
-                    // if statement to make when H is selected home will equal true
 
+                            case "H":
+                                ledger = false;
+                                home = true;
+                        }
+                        // if statement to make when H is selected home will equal true
+                        //create loops to make report\
+                    }
                     break;
+
 
                 case "X":
 
@@ -159,7 +176,7 @@ public class Capstone_1 {
                             "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢃⠘⡄⠀⠀⠉⠀⠀⣠⣾⠁⠀⠀⣧⣿⣿⡿⠃⠸⠿⣿⣿⣿⣿⣿⣿⠟⠁⣼⣾⠀⠀⠀⠀⢠⠇⠀⠀\n" +
                             "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⡄⠹⣀⣀⣤⣶⣿⡿⠃⠀⠀⠀⠈⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠁⠀⠀⢻⣿⣷⣦⣤⣤⠎⠀⠀⠀\n" +
                             "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣤⣿⡿⠟⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠀⠀");
-                        System.exit(0);
+                    System.exit(0);
                     break;
 
                 default:
@@ -169,5 +186,45 @@ public class Capstone_1 {
             }
         }
     }
+
+    static void handlePayments() {
+        try {
+            System.out.println("Im going to need your debit information");
+            System.out.println("Please enter your information in the following format");
+            System.out.println("2023-04-15|10:13:25|ergonomic keyboard|Amazon|-89.50");
+            String paymentInfo = input.nextLine();
+
+
+            paymentList.add(paymentInfo);
+
+            writer.write("\n" + paymentInfo);
+            writer.close();
+
+        } catch (RuntimeException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static void handleDeposit() {
+        try {
+            System.out.println("Im going to need your deposit information");
+            System.out.println("Please enter all the info in this exact format (MM/dd/yyyy)|(Military time e.g 17:00)| description | amount");
+            System.out.println("e.g 2023-04-15|11:15:00|Invoice 1001 paid|Joe|1500.00 ");
+            String depositInfo = input.nextLine();
+            //split using |
+
+            depositList.add(depositInfo);
+
+            writer.write("\n" + (depositInfo));
+            writer.close();
+
+        } catch (RuntimeException | IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
+
+
+
 
